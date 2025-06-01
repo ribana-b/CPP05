cafd65339666b4379f264f1fee9faae41cce0bc3
SrVariable <rojohnibana@gmail.com>
2025-06-01 23:19:04 +0200

    Add more Bureaucrat tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 20:34:18 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2025/06/01 15:42:03 by ribana-b         ###   ########.com      */
+/*   Updated: 2025/06/01 23:12:05 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,109 @@
 #include <iostream> // For std::cout, std::cerr
 
 // Helper macro to pretty-print tests
-#define PP_TEST(i) do{\
+#define PP_TEST(testIndex) do{\
 	std::string testName((__func__));\
-	std::cout << "\033[38;2;255;128;255m- Test " << (i)\
+	std::cout << "\033[38;2;255;128;255m- Test " << (testIndex)\
 	<< " (" << testName.substr(4) << "):\033[0m" << std::endl;\
 } while(0)
 
 // Helper macro to pretty-print caught exceptions
-#define PP_EXCEPTION(e) do{\
+#define PP_EXCEPTION(exception) do{\
 	std::cerr << "\033[31m" << (__FILE__) << ":" << (__LINE__) << "\033[0m"\
-	<< ": Exception caught. Reason:\033[0m " << (e).what() << std::endl;\
+	<< ": Exception caught. Reason:\033[0m " << (exception).what() << std::endl;\
 } while(0)
+
+// Helper macro to pretty-print undefined exceptions
+#define PP_UNDEFINED_EXCEPTION(message) do{\
+	std::cerr << "\033[31m" << (__FILE__) << ":" << (__LINE__) << "\033[0m"\
+	<< ": Undefined Exception caught. Reason:\033[0m " << (message) << std::endl;\
+} while(0)
+
+// Added later, and I don't want to rename every bureaucrat...
+void testSetBureaucratHighestGrade(std::size_t testIndex)
+{
+	PP_TEST(testIndex);
+
+	try
+	{
+		Bureaucrat artisfo("Artisfo", Bureaucrat::HIGHEST_GRADE);
+
+		std::cout << artisfo << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		PP_EXCEPTION(e);
+	}
+}
+
+// Added later, and I don't want to rename every bureaucrat...
+void testSetBureaucratLowestGrade(std::size_t testIndex)
+{
+	PP_TEST(testIndex);
+
+	try
+	{
+		Bureaucrat bluqfo("Bluqfo", Bureaucrat::LOWEST_GRADE);
+
+		std::cout << bluqfo << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		PP_EXCEPTION(e);
+	}
+}
+
+// Added later, and I don't want to rename every bureaucrat...
+void testBureaucratIncreaseGradeInBounds(std::size_t testIndex)
+{
+	PP_TEST(testIndex);
+
+	try
+	{
+		Bureaucrat ceniafo("Ceniafo", Bureaucrat::LOWEST_GRADE);
+		const int amount = 20;
+
+		std::cout << ceniafo << std::endl;
+		for (int i = 0; i < amount; ++i)
+		{
+			ceniafo.increaseGrade();
+		}
+		if (ceniafo.getGrade() != Bureaucrat::LOWEST_GRADE - amount)
+		{
+			PP_UNDEFINED_EXCEPTION("Grades doesn't match");
+		}
+	}
+	catch (const std::exception& e)
+	{
+		PP_EXCEPTION(e);
+	}
+}
+
+// Added later, and I don't want to rename every bureaucrat...
+void testBureaucratDecreaseGradeInBounds(std::size_t testIndex)
+{
+	PP_TEST(testIndex);
+
+	try
+	{
+		Bureaucrat deronfo("Deronfo", Bureaucrat::HIGHEST_GRADE);
+		const int amount = 20;
+
+		std::cout << deronfo << std::endl;
+		for (int i = 0; i < amount; ++i)
+		{
+			deronfo.decreaseGrade();
+		}
+		if (deronfo.getGrade() != Bureaucrat::HIGHEST_GRADE + amount)
+		{
+			PP_UNDEFINED_EXCEPTION("Grades doesn't match");
+		}
+	}
+	catch (const std::exception& e)
+	{
+		PP_EXCEPTION(e);
+	}
+}
 
 void testSetBureaucratGradeTooHigh(std::size_t testIndex)
 {
@@ -224,6 +316,10 @@ int	main()
 {
 	std::size_t testIndex = 0;
 
+	testSetBureaucratHighestGrade(testIndex++);
+	testSetBureaucratLowestGrade(testIndex++);
+	testBureaucratIncreaseGradeInBounds(testIndex++);
+	testBureaucratDecreaseGradeInBounds(testIndex++);
 	testSetBureaucratGradeTooHigh(testIndex++);
 	testSetBureaucratGradeTooLow(testIndex++);
 	testIncreaseBureaucrateGradeOverBounds(testIndex++);
